85f1342b4de29f935abc6133198f43a4742a8bd1
Naty89 <37753984+Naty89@users.noreply.github.com>
2020-11-11 20:00:24 -0500

    Update bfp2.py

--- bfp2.py
+++ bfp2.py
@@ -95,4 +95,113 @@ class BodyInfo:
                 if BFP > int(value[0]) and BFP < int(value[-1]):
                     return('You are in the {} category'.format(key))
 
-   
+    """
+    This method calculates the amount of 
+    pounds you need to lose to go down a category.
+    
+    I am going to be using three function inside
+    this method so that it is more organized
+    the three functions will be one that finds what category
+    your in, another one that will find what category is down and BFP
+    to be in that category, the third one calculates the how
+    pounds you need to lose.
+    """
+    def Pounds_To_Lose(self):
+
+        """
+        This function tells you what category your
+        are in
+        """
+        def category_in():
+            """
+            This part of the code calls out the US_Navy_Method method
+            to get the BFP
+            """
+            BFP = round(self.US_Navy_Method(), 1)
+            """
+            This makes a dictionary of the category
+            and the body percentage associated with it.
+            """
+            df = pd.read_csv('BFP_Catergories.csv')
+            female = {}
+            male = {}
+            for i in range(len(df.Description)):
+                female[df.Description[i]] = df.Women[i].replace('%', '').replace('+', '').split('-')
+            for i in range(len(df.Description)):
+                male[df.Description[i]] = df.Men[i].replace('%', '').replace('+', '').split('-')
+
+            """
+            This tells what category you are in 
+            """
+            if self.gender.lower() == 'male':
+                for key, value in male.items():
+                    if BFP > int(value[0]) and BFP < int(value[-1]):
+                        return(key)
+            if self.gender.lower() == 'female':
+                for key, value in female.items():
+                    if BFP > int(value[0]) and BFP < int(value[-1]):
+                        return(key)
+
+        """
+        This function tells you what category is below.
+        Example:
+        If you are in the Fitness category then the category
+        below you is Athelete category.
+        """
+        def category_below():
+            """
+            This makes a dictionary of the category
+            and the body percentage associated with it.
+            """
+            df = pd.read_csv('BFP_Catergories.csv')
+            female = {}
+            male = {}
+            for i in range(len(df.Description)):
+                female[df.Description[i]] = df.Women[i].replace('%', '').replace('+', '').split('-')
+            for i in range(len(df.Description)):
+                male[df.Description[i]] = df.Men[i].replace('%', '').replace('+', '').split('-')
+
+            y = list(female.keys())
+            x = list(male.keys())
+            if self.gender == 'male':
+                for i in range(len(x)):
+                    if x[i] == category_in():
+                        return int(male[x[i-1]][-1])
+            else:
+                for i in range(len(y)):
+                    if y[i] == category_in():
+                        return int(female[y[i-1]][-1])
+
+
+        """
+        This tells you how many pounds you need to loose 
+        to go down a category.
+        """
+        def pounds_to_lose():
+            pre_bfp = round(self.US_Navy_Method(), 1)
+            af_bfp = round(self.US_Navy_Method(), 1)
+            cb = category_below()
+            if self.gender.lower() == 'male':
+                if pre_bfp <= 5:
+                    "You shouldn't be losing any more weight."
+                else:
+                    while af_bfp > cb:
+                        af_bfp -= 0.2
+                    return "You have to lose {} pounds".format(round(round(round(pre_bfp-round(af_bfp, 1), 1)/0.2)*0.8))
+            else:
+                if pre_bfp <= 13:
+                    "You shouldn't be losing any more weight."
+                else:
+                    while af_bfp > cb:
+                        af_bfp -= 0.2
+
+                    return "You have to lose {} pounds".format(round(round(round(pre_bfp-round(af_bfp, 1), 1)/0.2)*0.8))
+
+
+
+        return(pounds_to_lose())
+        
+        
+
+x = BodyInfo(146, 65.5, 'male', 40, 12.5, 36.5, 36.5)
+print(x.Pounds_To_Lose())
